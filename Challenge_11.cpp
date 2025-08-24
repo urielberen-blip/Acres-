91ec7f58bbf2359c444b2a64df90f13ec0116238
urielberen-blip <urielberen@gmail.com>
2025-08-24 19:17:04 -0300

    Challenge_11.cpp

---
+++ Challenge_11.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+double distance_town = 20*23.5;
+double distance_highway = 20*28.9;
+
+int main() {
+    cout <<"The average distance traveled by a car when driven in town is "<< distance_town << endl;
+      cout <<"The average distance traveled by a car when driven in the highway is "<< distance_highway << endl;
+    return 0;
+}
